2db3b9de911d56784bd3ddf02a561a5fe9093502
Malik186 <ianmathew186@gmail.com>
2024-09-17 16:07:38 +0300

    Makes an update to support multiple Countries

--- dashboard/pages/overview/content/location/location.js.php
+++ dashboard/pages/overview/content/location/location.js.php
@@ -1,6 +1,19 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
    $(document).ready(function() {
+    // Define the list of countries and their coordinates
+    var countries = {
+        "Kenya": { latLng: [-1.286389, 36.817223], activityTotal: 0 },
+        "Uganda": { latLng: [0.347596, 32.582520], activityTotal: 0 },
+        "Tanzania": { latLng: [-6.792354, 39.208328], activityTotal: 0 },
+        "South Africa": { latLng: [-25.747868, 28.229271], activityTotal: 0 },
+        "Egypt": { latLng: [30.044420, 31.235712], activityTotal: 0 },
+        "Morocco": { latLng: [33.573110, -7.589843], activityTotal: 0 },
+        "USA": { latLng: [37.090240, -95.712891], activityTotal: 0 },
+        "England": { latLng: [51.509865, -0.118092], activityTotal: 0 },
+        "Australia": { latLng: [-33.868820, 151.209290], activityTotal: 0 }
+    };
+
     // Fetch data from the API using AJAX
     $.ajax({
         url: 'https://complytech.mdskenya.co.ke/endpoint/engine/activity.overview.php',
@@ -9,20 +22,44 @@
         success: function(response) {
             if (response.status === "success") {
                 var data = response.data;
-                var kenyaActivityTotal = 0;
 
-                // Loop through the data to sum up Activity for Tax_Type containing 'Kenya'
+                // Loop through the data to sum up Activity for each country in the list
                 data.forEach(function(item) {
                     var taxType = item.Tax_Type;
                     var activity = parseInt(item.Activity); // Assuming Activity is a number
 
-                    // Check if the Tax_Type contains "Kenya"
-                    if (taxType.includes("Kenya")) {
-                        kenyaActivityTotal += activity;
+                    // Check if the Tax_Type contains the name of any country in our list
+                    for (var country in countries) {
+                        if (taxType.includes(country)) {
+                            countries[country].activityTotal += activity;
+                        }
                     }
                 });
 
-                // Modify the map to plot Kenya and display the summed Activity
+                // Update the world map with markers for each country with its respective summed activity
+                var markers = [];
+                for (var country in countries) {
+                    if (countries[country].activityTotal > 0) {
+                        markers.push({
+                            latLng: countries[country].latLng,
+                            name: country,
+                            style: { fill: '#28C76F', r: 7 },
+                            description: 'Total Activity: ' + countries[country].activityTotal
+                        });
+
+                        // Dynamically update the HTML to display each country's activity in the card
+                        $('#activity-cards').append(`
+                            <div class="col">
+                                <div class="d-flex justify-content-between">
+                                    <p><span class="badge badge-success badge-dot me-5"></span>${country}</p>
+                                    <p>${countries[country].activityTotal}</p>
+                                </div>
+                            </div>
+                        `);
+                    }
+                }
+
+                // Initialize the map with the dynamically generated markers
                 $('#location').vectorMap({
                     map: 'world_mill_en',
                     backgroundColor: '#ffffff00',
@@ -43,19 +80,11 @@
                             'stroke': '#000',
                             'stroke-width': 1,
                             'stroke-opacity': 0.0
-                        },
+                        }
                     },
                     enableZoom: false,
                     hoverColor: '#bcc3fb',
-                    markers: [
-                        // Plot Kenya's coordinates with the summed Activity value
-                        {
-                            latLng: [-1.286389, 36.817223], // Coordinates for Nairobi, Kenya
-                            name: 'Kenya',
-                            style: { fill: '#28C76F', r: 7 }, // Style Kenya marker
-                            description: 'Total Activity: ' + kenyaActivityTotal // Add the activity total as a description
-                        }
-                    ],
+                    markers: markers,
                     hoverOpacity: null,
                     normalizeFunction: 'linear',
                     scaleColors: ['#b6d6ff', '#005ace'],
@@ -67,17 +96,6 @@
                         alert(message);
                     }
                 });
-
-                // Update the HTML to display Kenya's Activity in the card
-                $('#kenya-activity-card').html(`
-                <div class="col">
-                    <div class="d-flex justify-content-between">
-                        <p><span class="badge badge-success badge-dot me-5"></span>Kenya</p>
-                        <p>${kenyaActivityTotal}</p>
-                    </div>
-                </div>
-                `);
-
             } else {
                 console.log("Error fetching data from the API");
             }
--- dashboard/pages/overview/content/location/location.php
+++ dashboard/pages/overview/content/location/location.php
@@ -1,4 +1,4 @@
-<div class="col-xl-12 col-lg-12 col-12">					
+<div class="col-xl-12 col-lg-6 col-12">					
 					<div class="box">
 						<div class="box-header no-border pb-0">
 							<h4 class="box-title">Distribution Of Activities</h4>
@@ -6,7 +6,7 @@
 						<div class="box-body">
 							<div id="location" class="map-home h-220"></div>
 							<div class="row row-cols-2 mt-30">
-								<div id="kenya-activity-card">
+								<div id="activity-cards">
 								
 								</div>
 							</div>
