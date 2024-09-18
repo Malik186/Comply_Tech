550712af408f42bcd4a6fb7836eaebcb68f2c042
Malik186 <ianmathew186@gmail.com>
2024-09-18 15:12:57 +0300

    Updates the contents of the landing page

--- dashboard/pages/engine/content/content.php
+++ dashboard/pages/engine/content/content.php
@@ -1,9 +1,17 @@
 <div class="container-full">
   <section class="content">
     <div class="row">
-      <!-- sales stats-->
+      <!-- Cards stats-->
     <?php
     include_once site . "/dashboard/pages/engine/content/modal_form/select.rule.php";
+    ?>
+
+    <?php
+    include_once site . "/dashboard/pages/engine/content/modal_form/card/card.info.php";
+    ?>
+
+  <?php
+    include_once site . "/dashboard/pages/engine/content/modal_form/supported_countries/supported.countries.php";
     ?>
 
   </section>
