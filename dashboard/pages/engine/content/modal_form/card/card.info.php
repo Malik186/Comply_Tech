c1b6af9fcf6c1fd10ed4a4e4858d6ce8f892eb94
Malik186 <ianmathew186@gmail.com>
2024-09-18 15:13:26 +0300

    Includes a features card

---
+++ dashboard/pages/engine/content/modal_form/card/card.info.php
@@ -0,0 +1,37 @@
+<div class="col-xl-6">
+    <div class="box">
+        <div class="box-body d-flex p-0">
+        <div class="flex-grow-1 p-30 flex-grow-1 bg-img" style="background-color: #fa2627; background-position: calc(100% + 0.5rem) bottom; background-size: auto 100%; background-image: url(/dashboard/img/offer.png)">
+
+        <h4 class="fw-500">ComplyTech Offers</h4>
+
+            <div class="mt-5">
+                <div class="d-flex mb-10 fs-16">
+                    <span class="icon-Arrow-right mt-5 me-10"><span class="path1"></span><span class="path2"></span></span>
+                    <span class="text-white">Detailed Analytics</span>
+                </div>
+
+                <div class="d-flex mb-10 fs-16">
+                    <span class="icon-Arrow-right mt-5 me-10"><span class="path1"></span><span class="path2"></span></span>
+                    <span class="text-white">Comprehensive Reports</span>
+                </div>
+
+                <div class="d-flex mb-10 fs-16">
+                    <span class="icon-Arrow-right mt-5 me-10"><span class="path1"></span><span class="path2"></span></span>
+                    <span class="text-white">Invoice Management</span>
+                </div>
+
+                <div class="d-flex mb-10 fs-16">
+                    <span class="icon-Arrow-right mt-5 me-10"><span class="path1"></span><span class="path2"></span></span>
+                    <span class="text-white">Payroll Processing</span>
+                </div>
+
+                <div class="d-flex mb-10 fs-16">
+                    <span class="icon-Arrow-right mt-5 me-10"><span class="path1"></span><span class="path2"></span></span>
+                    <span class="text-white">Tax Summaries</span>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
