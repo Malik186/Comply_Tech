11574283d8f7fd29dce6452886c49c0c972a603c
Malik186 <ianmathew186@gmail.com>
2024-09-18 15:14:32 +0300

    creates a map to show the countries supported

---
+++ dashboard/pages/engine/content/modal_form/supported_countries/supported.countries.php
@@ -0,0 +1,13 @@
+<div class="col-xl-12 col-lg-6 col-12">
+    <div class="box">
+    <div class="box-header with-border">
+        <h4 class="box-title">ComplyTech Supported Countries</h4>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        <div id="world-map-markers" style="height: 400px"></div>
+    </div>
+    <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+</div>
