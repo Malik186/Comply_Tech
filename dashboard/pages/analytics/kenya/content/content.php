ca79a5c9f93479cc4101b6310cd7bf664d99b04a
Malik186 <ianmathew186@gmail.com>
2024-09-19 12:21:45 +0300

    creates the analytics page for paye results

---
+++ dashboard/pages/analytics/kenya/content/content.php
@@ -0,0 +1,10 @@
+<div class="container-full">
+  <section class="content">
+    <div class="row">
+    <!-- Analytics-->
+    <?php
+    include_once site . "/dashboard/pages/analytics/kenya/content/Paye_analytics/paye.analytics.php";
+    ?>
+    </div>
+  </section>
+</div>
