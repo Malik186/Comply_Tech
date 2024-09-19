b6fef35c55a66b4b60767153a8bd318cbd3ce5c6
Malik186 <ianmathew186@gmail.com>
2024-09-19 12:20:22 +0300

    contains scripts for the analytics page

---
+++ js/pages/dashboard2.js
@@ -0,0 +1,210 @@
+//[Dashboard Javascript]
+
+//Project:	ComplyTech
+//Primary use:   Used only for the main dashboard
+
+
+$(function () {
+
+    'use strict';
+      
+          var bar = new ProgressBar.Circle(progressbar1, {
+            color: '#3699ff',
+            // This has to be the same size as the maximum width to
+            // prevent clipping
+            strokeWidth: 30,
+            trailWidth: 5,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {
+              autoStyleContainer: false
+            },
+            from: { color: '#3699ff', width: 4 },
+            to: { color: '#3699ff', width: 4 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+              circle.path.setAttribute('stroke', state.color);
+              circle.path.setAttribute('stroke-width', state.width);
+  
+              var value = Math.round(circle.value() * 150);
+              if (value === 0) {
+                circle.setText('');
+              } else {
+                circle.setText("<i class='fa fa-building'></i>");
+              }
+  
+            }
+          });
+          bar.text.style.fontSize = '1.5rem';
+  
+          bar.animate(0.78);
+      
+      
+          var bar = new ProgressBar.Circle(progressbar2, {
+            color: '#EA5455',
+            // This has to be the same size as the maximum width to
+            // prevent clipping
+            strokeWidth: 30,
+            trailWidth: 5,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {
+              autoStyleContainer: false
+            },
+            from: { color: '#EA5455', width: 4 },
+            to: { color: '#EA5455', width: 4 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+              circle.path.setAttribute('stroke', state.color);
+              circle.path.setAttribute('stroke-width', state.width);
+  
+              var value = Math.round(circle.value() * 150);
+              if (value === 0) {
+                circle.setText('');
+              } else {
+                circle.setText("<i class='fa fa-hospital'></i>");
+              }
+  
+            }
+          });
+          bar.text.style.fontSize = '1.5rem';
+  
+          bar.animate(0.5);
+      
+      
+          var bar = new ProgressBar.Circle(progressbar3, {
+            color: '#FF9F43',
+            // This has to be the same size as the maximum width to
+            // prevent clipping
+            strokeWidth: 30,
+            trailWidth: 5,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {
+              autoStyleContainer: false
+            },
+            from: { color: '#FF9F43', width: 4 },
+            to: { color: '#FF9F43', width: 4 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+              circle.path.setAttribute('stroke', state.color);
+              circle.path.setAttribute('stroke-width', state.width);
+  
+              var value = Math.round(circle.value() * 150);
+              if (value === 0) {
+                circle.setText('');
+              } else {
+                circle.setText("<i class='fa fa-coins'></i>");
+              }
+  
+            }
+          });
+          bar.text.style.fontSize = '1.5rem';
+  
+          bar.animate(0.4);
+      
+      
+      
+          var bar = new ProgressBar.Circle(progressbar4, {
+            color: '#28C76F',
+            // This has to be the same size as the maximum width to
+            // prevent clipping
+            strokeWidth: 30,
+            trailWidth: 5,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {
+              autoStyleContainer: false
+            },
+            from: { color: '#28C76F', width: 4 },
+            to: { color: '#28C76F', width: 4 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+              circle.path.setAttribute('stroke', state.color);
+              circle.path.setAttribute('stroke-width', state.width);
+  
+              var value = Math.round(circle.value() * 150);
+              if (value === 0) {
+                circle.setText('');
+              } else {
+                circle.setText("<i class='fa fa-house'></i>");
+              }
+  
+            }
+          });
+          bar.text.style.fontSize = '1.5rem';
+  
+          bar.animate(0.3);
+      
+      
+      
+          var bar = new ProgressBar.Circle(progressbar5, {
+            color: '#3699ff',
+            // This has to be the same size as the maximum width to
+            // prevent clipping
+            strokeWidth: 30,
+            trailWidth: 5,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {
+              autoStyleContainer: false
+            },
+            from: { color: '#3699ff', width: 4 },
+            to: { color: '#3699ff', width: 4 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+              circle.path.setAttribute('stroke', state.color);
+              circle.path.setAttribute('stroke-width', state.width);
+  
+              var value = Math.round(circle.value() * 150);
+              if (value === 0) {
+                circle.setText('');
+              } else {
+                circle.setText("<i class='fa fa-money'></i>");
+              }
+  
+            }
+          });
+          bar.text.style.fontSize = '1.5rem';
+  
+          bar.animate(0.25);
+      
+      
+      
+          var bar = new ProgressBar.Circle(progressbar6, {
+            color: '#7367F0',
+            // This has to be the same size as the maximum width to
+            // prevent clipping
+            strokeWidth: 30,
+            trailWidth: 5,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {
+              autoStyleContainer: false
+            },
+            from: { color: '#7367F0', width: 4 },
+            to: { color: '#7367F0', width: 4 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+              circle.path.setAttribute('stroke', state.color);
+              circle.path.setAttribute('stroke-width', state.width);
+  
+              var value = Math.round(circle.value() * 150);
+              if (value === 0) {
+                circle.setText('');
+              } else {
+                circle.setText("<i class='fa fa-house'></i>");
+              }
+  
+            }
+          });
+          bar.text.style.fontSize = '1.5rem';
+  
+          bar.animate(0.15);
+      
+      
+      
+      
+      
+      
+  }); // End of use strict
