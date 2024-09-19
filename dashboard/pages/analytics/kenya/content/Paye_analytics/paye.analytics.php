cbb2dc8f0e0174b13a0a68ed085d8f5021f04224
Malik186 <ianmathew186@gmail.com>
2024-09-19 12:22:18 +0300

    Updates a bar and doughnut charts for the page

---
+++ dashboard/pages/analytics/kenya/content/Paye_analytics/paye.analytics.php
@@ -0,0 +1,110 @@
+<div class="col-12">
+	<div class="box">
+		<div class="box-header no-border pb-0">
+			<h4 class="box-title">Spend Overview</h4>
+		</div>
+		<div class="box-body">
+			<div class="row align-items-center">
+				<div class="col-xl-8 col-12">
+					<div id="deductions_trend"></div>
+				</div>
+				<div class="col-xl-4 col-12">
+					<div class="d-flex align-items-center justify-content-evenly b-1 h-300">
+						<div>
+							<div class="text-start mb-50 net-pay">
+								<h5>NET PAY</h5>
+								<h2 class="fw-600">Ksh 185k <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 2%</span></h2>
+							</div>
+							<div class="text-start gross-pay">
+								<h5>GROSS PAY</h5>
+								<h2 class="fw-600">Ksh 40k<span class="fs-16 mx-10 text-success"><i class="fa fa-arrow-up"></i> 6%</span></h2>
+							</div>
+						</div>
+						<div>
+							<div class="text-start mb-50 deductions">
+								<h5>DEDUCTIONS</h5>
+								<h2 class="fw-600">Ksh 5K  <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 2%</span></h2>
+							</div>
+							<div class="text-start savings">
+								<h5>SAVINGS</h5>
+								<h2 class="fw-600">Ksh 780K<span class="fs-16 mx-10 text-success"><i class="fa fa-arrow-up"></i> 6%</span></h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row mt-30">
+				<div class="col-xl-4 col-lg-6 col-12">
+					<div class="d-flex align-items-center">
+						<div>
+							<div id="progressbar1" class="mx-auto w-100 my-10 position-relative"></div>
+						</div>
+						<div class="text-start">
+							<h5>NSSF</h5>
+							<h3 class="fw-500">Ksh 85,254 <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 3%</span></h3>
+						</div>
+					</div> 
+				</div>
+				<div class="col-xl-4 col-lg-6 col-12">
+					<div class="d-flex align-items-center">
+						<div>
+							<div id="progressbar2" class="mx-auto w-100 my-10 position-relative"></div>
+						</div>
+						<div class="text-start">
+							<h5>NHIF</h5>
+							<h3 class="fw-500">Ksh 48,254 <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 1%</span></h3>
+						</div>
+					</div> 
+				</div>
+				<div class="col-xl-4 col-lg-6 col-12">
+					<div class="d-flex align-items-center">
+						<div>
+							<div id="progressbar3" class="mx-auto w-100 my-10 position-relative"></div>
+						</div>
+						<div class="text-start">
+							<h5>PAYE</h5>
+							<h3 class="fw-500">Ksh 11,254 <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 2%</span></h3>
+						</div>
+					</div> 
+				</div>
+				<div class="col-xl-4 col-lg-6 col-12">
+					<div class="d-flex align-items-center">
+						<div>
+							<div id="progressbar4" class="mx-auto w-100 my-10 position-relative"></div>
+						</div>
+						<div class="text-start">
+							<h5>Housing Levy</h5>
+							<h3 class="fw-500">Ksh 254 <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 1%</span></h3>
+						</div>
+					</div> 
+				</div>
+				<div class="col-xl-4 col-lg-6 col-12">
+					<div class="d-flex align-items-center">
+						<div>
+							<div id="progressbar5" class="mx-auto w-100 my-10 position-relative"></div>
+						</div>
+						<div class="text-start">
+							<h5>Insurance</h5>
+							<h3 class="fw-500">Ksh 454 <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 1%</span></h3>
+						</div>
+					</div> 
+				</div>
+				<div class="col-xl-4 col-lg-6 col-12">
+					<div class="d-flex align-items-center">
+						<div>
+							<div id="progressbar6" class="mx-auto w-100 my-10 position-relative"></div>
+						</div>
+						<div class="text-start">
+							<h5>Mortgage</h5>
+							<h3 class="fw-500">Ksh 754 <span class="fs-16 mx-10 text-fade"><i class="fa fa-arrow-up"></i> 2%</span></h3>
+						</div>
+					</div> 
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+
+	<?php
+    include_once site . "/dashboard/pages/analytics/kenya/content/Paye_analytics/analytics.js.php";
+    ?>
