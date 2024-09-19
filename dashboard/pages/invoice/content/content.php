3cb97a0ab2afbb2acffc35fca52924aa6e4ca708
Malik186 <ianmathew186@gmail.com>
2024-09-19 16:50:51 +0300

    creates an invoice page

---
+++ dashboard/pages/invoice/content/content.php
@@ -0,0 +1,10 @@
+<div class="container-full">
+  <section class="invoice printableArea">
+    
+      <!-- Invoice -->
+    <?php
+    include_once site . "/dashboard/pages/invoice/content/form/form.php";
+    ?>
+    
+  </section>
+</div>
