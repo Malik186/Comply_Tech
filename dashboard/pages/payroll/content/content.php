65f512a2da100ef01ac23510a61f2cafa9429866
Malik186 <ianmathew186@gmail.com>
2024-09-20 15:00:57 +0300

    creates a payroll page

---
+++ dashboard/pages/payroll/content/content.php
@@ -0,0 +1,10 @@
+<div class="container-full">
+  <section class="content">
+    <div class="row">
+    <!-- Analytics-->
+    <?php
+    include_once site . "/dashboard/pages/payroll/content/analytics/analytics.php";
+    ?>
+    </div>
+  </section>
+</div>
