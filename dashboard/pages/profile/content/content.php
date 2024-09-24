07cd84a78c4e6fdd221a68631f97df29bbc8eded
Malik186 <ianmathew186@gmail.com>
2024-09-24 17:13:09 +0300

    creates a profie page

---
+++ dashboard/pages/profile/content/content.php
@@ -0,0 +1,15 @@
+<div class="container-full">
+  <section class="content">
+    <div class="row">
+      <!-- Profile Image-->
+    <?php
+    include_once site . "/dashboard/pages/profile/content/profile_image/profile.image.php";
+    ?>
+
+    <!-- Profile Edit-->
+    <?php
+    include_once site . "/dashboard/pages/profile/content/account_edit/account.edit.php";
+    ?>
+    </div>
+  </section>
+</div>
