eded02a8090791398475a7c43731b676b444eecb
Malik186 <ianmathew186@gmail.com>
2024-09-26 13:31:28 +0300

    cleans up the page

--- dashboard/pages/settings/content/update/log/activity.log.php
+++ dashboard/pages/settings/content/update/log/activity.log.php
@@ -14,24 +14,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">Chrome on Window</th>
-                        <td>192.168.111.111</td>
-                        <td>1/04/2020 : 10:10am</td>
-                        <td><a href="#" class="text-danger"><i class="fa fa-trash"></i></a></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Mozila on Window</th>
-                        <td>192.168.154.111</td>
-                        <td>1/03/2020 : 10:10am</td>
-                        <td><a href="#" class="text-danger"><i class="fa fa-trash"></i></a></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Safari on Mac</th>
-                        <td>192.168.895.111</td>
-                        <td>1/05/2020 : 10:10am</td>
-                        <td><a href="#" class="text-danger"><i class="fa fa-trash"></i></a></td>
-                    </tr>
+                    <!-- Log in activities displayed here-->
                     </tbody>
                 </table>
             </div>
