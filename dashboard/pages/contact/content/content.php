94fc6a3f4cb8ad22dd4a23f711f084866331ee98
Malik186 <ianmathew186@gmail.com>
2024-09-26 14:51:37 +0300

    Creates a contact page

---
+++ dashboard/pages/contact/content/content.php
@@ -0,0 +1,10 @@
+<div class="container-full">
+  <section class="content">
+    <div class="row">
+      <!-- Contact-->
+    <?php
+    include_once site . "/dashboard/pages/contact/content/form/form.php";
+    ?>
+    </div>
+  </section>
+</div>
---
+++ dashboard/pages/contact/content/form/form.php
@@ -0,0 +1,28 @@
+<div class="col-12">
+    <div class="box">
+        <div class="box-header with-border">
+            <h6 class="box-title">You can find all of the questions and answers abour secure your account</h6>
+        </div>
+        <form class="form">
+            <div class="box-body">
+                <h5 class="box-title text-info mb-0 mt-20"><i class="ti-file me-15"></i> Give us the details </h5>
+                <hr class="my-15">								
+                <div class="form-group">
+                    <label class="form-label">Select File</label>
+                    <label class="file">
+                    <input type="file" id="file">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label class="form-label">Message</label>
+                    <textarea rows="5" class="form-control" placeholder="Write your message"></textarea>
+                </div>
+            </div>
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-paper-plane"></i> Submit
+                </button>
+            </div>  
+        </form>
+        </div>
+</div>
